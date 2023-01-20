110dbe10236521bf7a6592d51f883a58226610f2
tush1103 <97387628+tush1103@users.noreply.github.com>
2023-01-20 17:04:07 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Components/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
